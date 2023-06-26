0856c58767d3f28fc0c1b907d2975502e34338b7
desolaafujah <desolafujah@icloud.com>
2023-06-26 22:04:36 +0000

    End of Day 1 Progress

--- movieCatalog/movies.py
+++ movieCatalog/movies.py
@@ -1,4 +1,5 @@
 import requests
+import sqlite3
 
 url = "https://covid-193.p.rapidapi.com/statistics"
 
@@ -7,6 +8,35 @@ headers = {
 	"X-RapidAPI-Host": "covid-193.p.rapidapi.com"
 }
 
+def createTable():
+    conn = sqlite3.connect("covid.db")
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS country_data(id INTEGER PRIMARY KEY AUTOINCREMENT, country TEXT, cases INTEGER)""")
+    conn.commit()
+    conn.close()
+
+def store(country, data):
+    cases = data["response"][0]["cases"]["total"]
+    conn = sqlite3.connect("covid.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO country_data (country, cases) VALUES (?, ?)", (country, cases))
+    conn.commit()
+    conn.close()
+
 response = requests.get(url, headers=headers)
 
-print(response.json())
+def getData(country):
+    response = requests.get(url, headers=headers, params={"country": country})
+    response.raise_for_status()
+    data = response.json()
+    cases = data["response"][0]["cases"]["total"]
+    store(country, data)
+    return data
+
+createTable()
+inputCountry = input("Enter a country: ")
+
+if(inputCountry):
+    returnData = getData(inputCountry)
+    if(returnData):
+        print(returnData)
